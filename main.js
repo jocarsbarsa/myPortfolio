1e2c3adfcd2e56112d87056f5e3ae19d8f826b2f
jocarsbarsa <86578210+jocarsbarsa@users.noreply.github.com>
2021-10-22 12:54:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// Criar barra
+/*let bar = document.createElement('div')
+
+// estilo da barra
+bar.style.height = "4px"
+bar.style.width = "500px"
+bar.style.backgroundColor = "blue"
+bar.style.position = "fixed"
+bar.style.top = "0"
+bar.style.top = "0"
+bar.style.left = "0"
+bar.style.zIndex = '9999'
+
+
+// adiciona no corpo da página
+document.body.append(bar)
+
+
+// atualizar a barra
+
+// verificar o movimento do scroll
+document.addEventListener("scroll", () => {
+  // o tamanho da caixa que contem o texto
+console.log("updating")
+
+  // verificar em que posição da pagina eu estou 
+
+
+  // regra de 3
+
+})*/
+/*$(function() {
+  var current_progress = 0;
+  var interval = setInterval(function() {
+      current_progress += 10;
+      $("#dynamic")
+      .css("width", current_progress + "%")
+      .attr("aria-valuenow", current_progress)
+      .text(current_progress + "% Complete");
+      if (current_progress >= 100)
+          clearInterval(interval);
+  }, 1000);
+});*/
+
